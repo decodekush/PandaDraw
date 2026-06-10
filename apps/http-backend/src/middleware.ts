6fc0e3f36e63dde1f550228e99167979d4bdb891
decodekush <kushagraagarwal1612@gmail.com>
2026-06-10 12:41:30 +0530

    Added authentication

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+
+interface AuthRequest extends Request {
+    userId?: string;
+}
+
+export function middleware(req: AuthRequest, res: Response, next: NextFunction) {
+    const token = req.headers['authorization'] ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (decoded && typeof decoded !== 'string' && 'userId' in decoded) {
+        req.userId = String(decoded.userId);
+        next();
+    } else {
+        res.status(403).json({
+            message: "Unauthorized"
+        });
+    }
+}
